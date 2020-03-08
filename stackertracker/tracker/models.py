dd84fbdf0b4f1ff1aa1e10d6df7ef91b8b4e0a6c
jlsphotos <admin@jlsphotos.co.uk>
2020-03-08 22:53:39 +0000

    added requirements

--- stackertracker/tracker/models.py
+++ stackertracker/tracker/models.py
@@ -1,28 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
 class Metals(models.Model):
     metal_short = models.CharField(max_length=3, unique=True)
     metal_name = models.CharField(max_length=25)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'Metal\'s'
+
     def __str__(self):
         return f'{self.metal_short} - {self.metal_name}'
+
 
 class Currencies(models.Model):
     currency_short = models.CharField(max_length=3)
     currency_name = models.CharField(max_length=25)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'Currencies'
+
     def __str__(self):
         return f'{self.currency_name} - {self.currency_short}'
+
 
 class Price_List(models.Model):
     metal = models.ForeignKey(Metals, on_delete=models.CASCADE)
     currency = models.ForeignKey(Currencies, on_delete=models.CASCADE)
     date = models.DateField()
-    price = models.DecimalField(decimal_places=3,max_digits= 6)
-
+    price = models.DecimalField(decimal_places=3, max_digits=6)
 
 
 class Transactions(models.Model):
